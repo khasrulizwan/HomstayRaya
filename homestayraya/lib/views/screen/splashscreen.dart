5ce36bd621a9290c2c47bee80c9f0d0cc0ea7729
khasrulizwan <khasrulizwan6@gmail.com>
2023-01-31 13:24:11 +0800

    homstayRaya

---
+++ homestayraya/lib/views/screen/splashscreen.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const Splashcreen());
+
+class Splashcreen extends StatelessWidget {
+  const Splashcreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Material App',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Material App Bar'),
+        ),
+        body: const Center(
+          child: Text('Hello World'),
+        ),
+      ),
+    );
+  }
+}
